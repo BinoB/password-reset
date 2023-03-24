1cda1838d0cab23ff733481a286d8ba0ea55d964
BinoB <binob831@gmail.com>
2023-03-24 12:49:09 +0530

    initial commit

---
+++ routes/userRoute.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const {
+  registerUser,
+  loginUser,
+  logout,
+  getUser,
+  loginStatus,
+  updateUser,
+  changePassword,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/userController");
+const protect = require("../middleWare/authMiddleware");
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logout);
+router.get("/loggedin", loginStatus);
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resetToken", resetPassword);
+
+module.exports = router;
